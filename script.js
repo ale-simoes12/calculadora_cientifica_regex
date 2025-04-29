6e328e2ad105d6958049975a6cbb89d13b607b32
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-04-29 16:09:20 -0300

    feat: adiciona event listener para entrada via teclado físico

--- script.js
+++ script.js
@@ -29,4 +29,45 @@ const mapeamentoTeclas = {
 
 let equacao = "";
 const entrada = document.getElementById('entrada');
+
+
+
+
+document.addEventListener("keydown", function (event) {
+    let teclaFisica = event.key;
+    const textoTeclaVirtual = mapeamentoTeclas[teclaFisica];
+    const teclasVirtuais = document.querySelectorAll(".tecla");
   
+    if (teclaFisica == "Backspace") {
+      equacao = equacao.slice(0, -1);
+      const botaoApagar = teclasVirtuais[3];
+      hoverTecla(botaoApagar);
+  
+    }
+  
+    else if (teclaFisica === "=") {
+      calcularEquacao();
+      event.preventDefault();
+      const botaoIgual = teclasVirtuais.length - 1
+      hoverTecla(teclasVirtuais[botaoIgual]);
+      return;
+    }
+  
+    else if (validarProximoCaractere(equacao, teclaFisica)) {
+      adicionaEquacao(teclasVirtuais, textoTeclaVirtual, teclaFisica, event);
+    }
+  
+    else {
+      event.preventDefault();
+    }
+  
+    atualizarDisplay();
+  });
+
+
+  function hoverTecla(tecla) {
+    tecla.classList.add("pressionada");
+    setTimeout(() => {
+      tecla.classList.remove("pressionada");
+    }, 200);
+  }
